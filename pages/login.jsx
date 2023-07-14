85410384a1c54177b90f1e7f018bc7167eba7810
Gissell10 <gissellbustosb@gmail.com.com>
2023-07-14 13:12:12 +1000

    add google provider

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,9 +1,24 @@
-import React from "react";
-import { UseSession, signIn, signOut, useSession } from "next-auth/react";
+import styles from "/components/home.module.css";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const login = () => {
   const { data: session } = useSession();
-  return <div>login</div>;
+
+  if (session) {
+    return (
+      <div className={styles.timeForm}>
+        <p>welcome,{session.user.email}</p>
+        <button onClick={() => signOut()}> Sign out</button>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.timeForm}>
+        <p> You aren't signed In</p>
+        <button onClick={() => signIn()}> Sign In</button>
+      </div>
+    );
+  }
 };
 
 export default login;
